ea7989357563d900138c6741f63435d285a27e1e
ldelenger <ldelenger.git@gmail.com>
2019-12-11 17:04:07 +0200

    updated examples

--- examples/big_power_of_two/main.c
+++ examples/big_power_of_two/main.c
@@ -11,19 +11,23 @@ int main(int argc, char ** argv){
 
 	uint64_t power = (uint64_t)atoll(argv[1]);
 
+	/*
+	 * Create a big integer object that will be to store the value of 2 ^ power 
+	 * (power >> 3) + (1 && (power & 0x7)) - is the actual amount of bytes needed to store the value
+	 */
 	bigint_t bigint = bigint_create((power >> 3) + (1 && (power & 0x7)));
 
+	// put the value of 1 in `bigint`
 	bigint_num_init(bigint, 1);
 
 	if(power)
-		bigint_shl(bigint, power);
+		bigint_shl(bigint, power); // raise bigint to `power` power
 	
-	printf("%s", bigint_to_string(bigint, 10));
-	printf("(0x%s)\n", bigint_to_string(bigint, 16));
+	printf("%s", bigint_to_string(bigint, 10)); // print `bigint` in decimal
+	printf("(0x%s)\n", bigint_to_string(bigint, 16)); // print `bigint` in hexidecimal
 
-	bigint_to_string(NULL, 0);
-	bigint_destroy(bigint);
-
+	bigint_to_string(NULL, 0); // free bigint_to_string()'s local buffer
+	bigint_destroy(bigint); // free `bigint`'s allocated memory
 
 	return 0;
 }
